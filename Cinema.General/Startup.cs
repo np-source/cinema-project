e7c11fc4c35d54c232585abbfcdad6b1d570a86a
rayonaru <vova.tuktanov@mail.ru>
2021-03-14 11:00:42 +0300

    clean

--- Cinema.General/Startup.cs
+++ Cinema.General/Startup.cs
@@ -1,4 +1,3 @@
-using Cinema.Models;
 using Cinema.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -17,14 +16,12 @@ namespace CinemaGeneral
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton(typeof(IRepository<>), typeof(MockRepository<>));
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,15 +31,12 @@ namespace CinemaGeneral
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
