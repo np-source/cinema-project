5753cb36bb54983fba0f268cf2da3efe0ac23f0f
rayonaru <vova.tuktanov@mail.ru>
2021-04-04 23:54:05 +0300

    fuck this

--- Cinema.General/Pages/Sessions/Sessions.cshtml.cs
+++ Cinema.General/Pages/Sessions/Sessions.cshtml.cs
@@ -2,11 +2,27 @@ using Cinema.Models;
 using Cinema.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cinema.General.Pages.Sessions
 {
     public class SessionsModel : PageModel
     {
+        class Place
+        {
+            public int Id { get; set; }
+        }
+
+        class Places
+        {
+            public List<Place> places { get; set; }
+        }
+
         private readonly ISessionRepository _db;
 
         public SessionsModel(ISessionRepository db)
@@ -20,17 +36,43 @@ namespace Cinema.General.Pages.Sessions
         {
             Session = _db.GetByID(id);
         }
-        
-        public void OnPostReservePlaces(int id)
+
+        //public void OnPostReservePlaces(int id)
+        //{
+        //    Session = _db.GetByID(id);
+
+        //    //foreach (var place in Session.Places)
+        //    //{
+        //    //    place.Position = 0;
+        //    //    place.Row = 0;
+        //    //}
+
+        //    _db.Update(Session);
+        //}
+
+        public ActionResult OnPostSend()
         {
-            Session = _db.GetByID(id);
-            foreach (var place in Session.Places)
+            Places places = null;
             {
-                place.Position = 0;
-                place.Row = 0;
+                var stream = new MemoryStream();
+                Request.Body.CopyTo(stream);
+                stream.Position = 0;
+                using (var reader = new StreamReader(stream))
+                {
+                    string requestBody = reader.ReadToEnd();
+
+                    if (requestBody.Length > 0)
+                    {
+                        var obj = JsonConvert.DeserializeObject<Places>(requestBody);
+
+                        if (obj != null)
+                            places = obj;
+
+                    }
+                }
             }
 
-            _db.Update(Session);
+            return new JsonResult(places);
         }
     }
 }
--- Cinema.General/Startup.cs
+++ Cinema.General/Startup.cs
@@ -2,6 +2,7 @@ using Cinema.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,10 +19,21 @@ namespace CinemaGeneral
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
-        {
+        {   // If using Kestrel:
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            // If using IIS:
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
             //services.AddSingleton(typeof(IRepository<>), typeof(MockRepository<>));
             services.AddScoped<IFilmRepository, MockFilmRepository>();
             services.AddScoped<ISessionRepository, MockSessionRepository>();
+            services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
             services.AddRazorPages();
 
             services.Configure<RouteOptions>(options =>
